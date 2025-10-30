b2f2c5515b97af3db50951e47078c304e2f687bc
zkkkol <j.kolesova1127@gmail.com>
2025-10-30 13:45:22 +0200

    Updated Deleted

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://dijusbubnys144_db_user:Sjrh1Gu88NO2w9JG@cluster0.nvd5lau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => console.log("connected to DB"))
 .catch((error) => console.log(error));
-const { createCourse, getallrecords, findOneCourse} = require("./coursescontrroller");
+const { createCourse, getallrecords, findOneCourse, updateCoursesById, deleteOneCourse} = require("./coursescontrroller");
 
 app.post("/courses", createCourse);
 app.get("/allcourses", getallrecords);
 app.get("/course/:id", findOneCourse);
+app.put("/course/:id", updateCoursesById)
+app.delete("/course/:id", deleteOneCourse)
 
 ////////////////////////////////////////////////////////////////////
 app.listen(5050, () => console.log("Serveris veikia, port 5050"));
--- coursescontrroller.js
+++ coursescontrroller.js
@@ -1,3 +1,4 @@
+const courses = require("./courses.js");
 const Course = require("./courses.js");
 
 console.log(Course)
@@ -49,5 +50,52 @@ const findOneCourse = async (req,res) => {
         res.status(500).json({error: "internal server error. Something went wrong"});
     }
 }
+////////////////////////////////////////
+// UPDATE
+// PUT
+const updateCoursesById = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const {title,teacher} = req.body;
+    
+        if (!title || !teacher) {
+            return res.status(400).json({error: "Title not found"});
+        }
+        const CoursesFromDB = await Course.findById(id);
 
-module.exports = {createCourse, getallrecords, findOneCourse};
+    if (!CoursesFromDB) {
+            return res.status(404).json({error: "Course not found"});
+    }
+        CoursesFromDB.title = title;
+        CoursesFromDB.teacher = teacher;
+    
+        const result = await CoursesFromDB.save();
+
+        res.status(200).json({message: "record updated succsesfully", result});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "internal server error. Something whent wrong"});
+    }
+    };
+
+////////////////////////////////////////
+// DELETE
+// DELETE
+const deleteOneCourse = async (req,res) => {
+    try {
+        const {id} =req.params;
+        
+        const result = await Course.findByIdAndDelete(id);
+
+        if (!result){
+            return res.status(404).json({error: "Course not found"});
+        }
+
+        res.status(200).json({message: "record deleted succsesfully", result});
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).json({error: "internal server error. Something whent wrong"});
+    }
+}
+module.exports = {createCourse, getallrecords, findOneCourse, updateCoursesById, deleteOneCourse};
